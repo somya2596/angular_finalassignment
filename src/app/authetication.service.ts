e796363c3d37ee673db8724e127e05c5cb475a18
diksha1786 <47427213+diksha1786@users.noreply.github.com>
2019-03-28 03:55:20 -1200

    Add files via upload

---
+++ src/app/authetication.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {Users} from  './users';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AutheticationService {
+
+  private _url : string ="https://detailform-6dced.firebaseio.com/detailForm.json";
+
+  constructor(private http: HttpClient) { }
+
+  getUsers():Observable<Users[]>
+  {
+   
+    return this.http.get<Users[]>(this._url);
+  }
+  setUser(userdata):Observable<Users[]>
+  {
+  console.log(userdata);
+    return this.http.post<Users[]>(this._url,userdata);
+  }
+  logout(): void {
+    localStorage.setItem('isLoggedIn', "false");
+    localStorage.removeItem('token');
+  } 
+}
